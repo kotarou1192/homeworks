50d976ddabc12d6c399162483ff4ec9e83c57145
kousuke_ida <takashiii1192@gmail.com>
2020-07-25 19:09:31 +0900

    update jest config and index.html and app.tsx

--- js-primer-todo-with-react/src/apps/app.tsx
+++ js-primer-todo-with-react/src/apps/app.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
+import { Todo, TodoProps } from "./todo";
 
-export const App = () => {
-  return <p>hello, world</p>;
+export const App: React.SFC = () => {
+  const [todos, setTodos] = React.useState<TodoProps[]>([]);
+
+  return (
+    <div>
+      <Todo text={"hoge"} deadline={new Date()} isDone={true} />
+    </div>
+  );
 };
