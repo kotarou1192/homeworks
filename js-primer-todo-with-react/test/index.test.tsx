1c7084cdaa3b0779c13816c15941d5e249f0d327
kousuke_ida <takashiii1192@gmail.com>
2020-07-25 14:59:22 +0900

    do hello world with react and jest

---
+++ js-primer-todo-with-react/test/index.test.tsx
@@ -0,0 +1,25 @@
+import { App } from "../src/apps/app";
+import { act } from "react-dom/test-utils";
+import ReactDOM, { unmountComponentAtNode } from "react-dom";
+import React from "react";
+
+let container: HTMLElement = document.createElement("div");
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+});
+
+describe("App test", () => {
+  test("should display hello, world", () => {
+    act(() => {
+      ReactDOM.render(<App />, container);
+    });
+    expect(container.textContent).toBe("hello, world");
+  });
+});
